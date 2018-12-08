d580deb04b57efb455707da846625a805b262b11
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-08 09:07:20 -0500

    First working version of package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,21 @@
 mkdir build
 cd build
+
+# The examples, tests, and docs don't really get installed
+# So we skip building them
+# Also note that the examples have a hard time finding the libraries that
+# need to be linked against.
+# In the past, I had to use
+# CFLAGS="${CFLAGS} -lm -lrt -ldl -lX11 -lpthread -lXrandr -lXinerama -lXcursor"
+# -DRT_LIBRARY=${PREFIX}/lib 
+# -DMATH_LIBRARY=${PREFIX}/lib
 cmake  \
     -DCMAKE_PREFIX_PATH=${PREFIX}     \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}  \
+    -DBUILD_SHARED_LIBS=ON            \
+    -DGLFW_BUILD_EXAMPLES=OFF         \
+    -DGLFW_BUILD_TESTS=OFF            \
+    -DGLFW_BUILD_DOCS=OFF             \
     ..
 
 make install
